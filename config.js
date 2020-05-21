bfb28e9d47278ca3728c7be26178056aedd61ac2
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-05-21 22:17:07 +0900

    [fix] prof

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© 2020 Yoshiki Ohashi All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-157574909-3',
   useKatex: false,
   menu: [
     {
@@ -39,7 +39,8 @@ module.exports = {
   author: {
     name: 'Yoshiki Ohashi',
     photo: 'https://avatars3.githubusercontent.com/u/20635620?s=400&u=280a071aeba97582646be64a5b75921fff99cfdf&v=4',
-    bio: '27歳のフリーランスエンジニア。SI企業1年, Webベンチャー企業2年で上流から下流のリードエンジニアを経て独立。 エンジニアらしく性格は温和。プロジェクトチームに心理的安全性を求める。最近はWebアプリ案件でWebflux、Spring Gatewayなどで実装。FWバンザイ。他にもWebsocketや文字数。',
+    bio: '27歳のフリーランスエンジニア。SI企業1年, Webベンチャー企業2年で上流から下流のリードエンジニアを経て独立。 エンジニアらしく性格は温和。プロジェクトチームに心理的安全性を求める。最近はWebアプリ案件で' +
+        'Spring5、Spring Cloud Gatewayで実装。',
     contacts: {
       email: 'rrrtcdish2@gmail.com',
       facebook: 'yoshiki.ohashi0428',
