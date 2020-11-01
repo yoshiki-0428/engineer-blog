adf13eac4b197b2b38ccbff64afef92322227611
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-11-01 23:46:09 +0900

    [add] hatena icon

--- src/components/ShareSns/ShareSns.js
+++ src/components/ShareSns/ShareSns.js
@@ -8,29 +8,54 @@ import TwitterIcon from 'react-share/es/TwitterIcon';
 import LineShareButton from 'react-share/es/LineShareButton';
 import LineIcon from 'react-share/es/LineIcon';
 import 'twin.macro';
+import Helmet from 'react-helmet';
 import { useSiteMetadata } from '../../hooks';
 
 export const ShareSns = ({ articleUrl, articleTitle }) => {
   const { author } = useSiteMetadata();
+  const iconSize = 40;
 
   return (
-      <div tw="text-center">
-        <div>
+      <div>
+        <Helmet>
+          <script type="text/javascript" src="//b.st-hatena.com/js/bookmark_button.js" charSet="utf-8" async="async"/>
+        </Helmet>
+
+        <div tw="flex justify-center">
+          <div tw="m-2">
+            <a
+              aria-label="share_hatena-bookmark"
+              href="http://b.hatena.ne.jp/entry/"
+              className="hatena-bookmark-button"
+              data-hatena-bookmark-layout="touch-counter"
+              title="このエントリーをはてなブックマークに追加"
+              style={{ boxShadow: 'none' }}
+            >
+              <img
+                src="https://b.st-hatena.com/images/entry-button/button-only@2x.png"
+                alt="このエントリーをはてなブックマークに追加"
+                width="24"
+                height="24"
+                style={{ border: 'none' }}
+              />
+            </a>
+          </div>
+
+          <TwitterShareButton tw="m-2" title={articleTitle} via={author.contacts.twitter} url={articleUrl}>
+            <TwitterIcon size={iconSize} round />
+          </TwitterShareButton>
+
+          <LinkedinShareButton tw="m-2" url={articleUrl}>
+            <LinkedinIcon title={articleTitle} size={iconSize} round />
+          </LinkedinShareButton>
+
           <FacebookShareButton tw="m-2" url={articleUrl}>
-            <FacebookIcon size={32} round />
+            <FacebookIcon size={iconSize} round />
           </FacebookShareButton>
 
           <LineShareButton tw="m-2" url={articleUrl}>
-            <LineIcon size={32} round />
+            <LineIcon size={iconSize} round />
           </LineShareButton>
-
-          <LinkedinShareButton tw="m-2" url={articleUrl}>
-            <LinkedinIcon title={articleTitle} size={32} round />
-          </LinkedinShareButton>
-
-          <TwitterShareButton tw="m-2" title={articleTitle} via={author.contacts.twitter} url={articleUrl}>
-            <TwitterIcon size={32} round />
-          </TwitterShareButton>
         </div>
       </div>
   );
