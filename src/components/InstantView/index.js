4d05b8db8620cb6d56849988d93d77ca2839957a
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-11-10 15:22:26 +0900

    fix icon

--- src/components/InstantView/index.js
+++ src/components/InstantView/index.js
@@ -3,8 +3,6 @@ import { Link } from 'gatsby';
 import tw from 'twin.macro';
 import ImageWrap from '../Image/ImageWrap';
 import { SPACER, TEXT_GATSBY_LINK_H3 } from '../Tailwind';
-import Icon from '../Icon';
-import { getIcon } from '../../utils';
 
 const InstantView = ({ items, flex }) => {
   const Width = flex ? tw.div`w-1/2` : tw.div`w-full`;
@@ -14,10 +12,7 @@ const InstantView = ({ items, flex }) => {
       <Div>
         {items.map((item, i) => (
             <Width key={i}>
-              <div tw="m-4 my-8 rounded overflow-hidden border border-base-gray-light bg-white relative">
-                <div tw="absolute top-0 left-0 z-10 bg-yellow-400">
-                  <Icon tw="" icon={getIcon('twitter')}></Icon>
-                </div>
+              <div tw="m-4 my-8 rounded overflow-hidden border border-base-gray-light bg-white">
                 <Link to={item.slug}>
                   <ImageWrap
                       item={{ socialImage: item.socialImage, alt: item.alt }}/>
